09df0ab575ffb1b293ff0fef8a1fcceba68b1f58
casper91044 <casper91044@mail.ru>
2020-05-09 14:31:57 +0300

    lesson 16

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import {Post} from "./Post/Post";
 import style from "./MyPosts.module.css"
-export function MyPosts() {
-    return <div className={style.item}>
-        My post
-        <div>new post</div>
-        <Post />
-    </div>
+export const MyPosts = () => {
+    return(
+        <div>
+            My posts
+            <div>
+                <textarea></textarea>
+                <button>Add post</button>
+            </div>
+            <div className={style.myposts}>
+                <Post message="Hi,how are you?"/>
+                <Post message="It is my first post!"/>
+
+            </div>
+        </div>
+
+    )
 }
 
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import style from "./Post.module.css";
 
-export function Post() {
-    return <div>
-        <div>post</div>
+
+
+export const Post = (props) => {
+
+    return <div className={style.item}>
+        <img src="https://html5css.ru/w3images/avatar2.png" alt="avatar"/>
+        {props.message}
+        <div>
+            <span>like</span>
+        </div>
     </div>
 }
