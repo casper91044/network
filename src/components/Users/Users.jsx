2525c53413bdc6cac38b099b253b12abc366f507
casper91044 <casper91044@mail.ru>
2020-06-23 19:58:19 +0300

    lesson 51

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,48 +1,24 @@
 import React from "react";
-import styles from  "./users.module.css"
+import styles from  "./users.module.css";
+import * as axios from 'axios';
+import userPhoto from '../../assets/img/users.png'
 
 export let Users = (props) => {
-    if (props.users.length === 0) {
-        props.setUsers([{
-            id: 1,
-            photoUrl: 'https://icon-icons.com/icons2/1879/PNG/72/iconfinder-7-avatar-2754582_120519.png',
-            followed: true,
-            fullName: 'Art',
-            status: 'I am a BOSS',
-            location: {city: 'Donetsk', country: 'DPR'}
-        },
-            {
-                id: 2,
-                photoUrl: 'https://icon-icons.com/icons2/1879/PNG/72/iconfinder-4-avatar-2754580_120522.png',
-                followed: false,
-                fullName: 'Elena',
-                status: 'I am a BOSS too',
-                location: {city: 'Donetsk', country: 'DPR'}
-            },
-            {
-                id: 3,
-                photoUrl: 'https://icon-icons.com/icons2/1879/PNG/72/iconfinder-10-avatar-2754575_120521.png',
-                followed: false,
-                fullName: 'Andru',
-                status: 'Driver',
-                location: {city: 'Donetsk', country: 'DPR'}
-            },
-            {
-                id: 4,
-                photoUrl: 'https://icon-icons.com/icons2/2129/PNG/72/black_man_beard_curly_hair_people_avatar_boy_person_icon_131372.png',
-                followed: false,
-                fullName: 'Kir',
-                status: 'Pub mobile',
-                location: {city: 'Donetsk', country: 'DPR'}
-            },
-        ])
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    props.setUsers(response.data.items);
+                });
+        }
     }
     return <div>
+        <button onClick={getUsers}>getUsers</button>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src= {u.photoUrl}/>
+                        <img src= {u.photos.small != null ? u.photos.small: userPhoto}/>
                     </div>
                     <div>
                         {u.followed
@@ -53,12 +29,12 @@ export let Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
                 </span>
             </div>)
